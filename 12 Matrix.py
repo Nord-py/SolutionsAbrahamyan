78920c998ca83400497efd5bee48b7b035535911
Nord <nikitin.2303@yandex.ru>
2022-02-20 23:50:06 +0400

    Create 12 Matrix.py
    Добавил 12 - Матрицы, с решением 15 и 16 задачи

---
+++ 12 Matrix.py
@@ -0,0 +1,376 @@
+import random
+from math import sqrt, sin, cos, e, pi
+import numpy as np
+
+'''
+Все обозначения из книги, читай условия.
+M - строка
+N - столбец
+'''
+
+
+def N1():
+    M = random.randrange(2,10)
+    N = random.randrange(2,10)
+    print("M = ",M,"; N = ",N)
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = (i+1)*10
+
+    print(a)
+
+def N2():
+    M = random.randrange(2,10)
+    N = random.randrange(2,10)
+    print("M = ",M,"; N = ",N)
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = (j+1)*5
+
+    print(a)
+
+def N3():
+    M = random.randrange(2,10)
+    N = random.randrange(2,10)
+    A = [random.randint(-10, 10) for i in range(M)]
+    print("M = ", M,"; N = ", N, 'A = ', A )
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = A[i]
+
+    print(a)
+
+
+def N4():
+    M = random.randrange(2,10)
+    N = random.randrange(2,10)
+    A = [random.randint(-10, 10) for i in range(N)]
+    print("M = ", M,"; N = ", N, 'A = ', A )
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = A[j]
+
+
+    print(a)
+
+
+def N5():
+    M = random.randrange(2,10)
+    N = random.randrange(2,10)
+    D = random.randrange(2,10)
+    A = [random.randint(-10, 10) for i in range(M)]
+    print("M = ", M,"; N = ", N, '; A = ', A, '; D = ', D )
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = A[i] + D * j
+
+    print(a)
+
+def N6():
+    M = random.randrange(2,10)
+    N = random.randrange(2,10)
+    D = random.randrange(2,10)
+    A = [random.randint(-10, 10) for i in range(N)]
+    print("M = ", M,"; N = ", N, '; A = ', A, '; D = ', D )
+   # a = np.zeros((M, N))
+    a = [[0 for i in range(N)] for i in range(M)]
+
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = A[j] * D**i
+
+
+    for i in a:
+        print(i)
+
+
+def N7():
+    M = random.randrange(2, 10)
+    N = random.randrange(2, 10)
+    K = random.randrange(1, M)
+
+    print("M = ", M, "; N = ", N, "; K = ", K)
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = j*i
+
+    print(a, '\n', '\n', a[K-1])
+
+
+def N8():
+    M = random.randrange(2, 10)
+    N = random.randrange(2, 10)
+    K = random.randrange(1, N)
+
+    print("M = ", M, "; N = ", N, "; K = ", K)
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = j*i
+
+    print(a, '\n', '\n')
+
+    for i in range(M):
+        print(a[i][K-1], end = ', ')
+
+
+def N9():
+    M = random.randrange(2, 10)
+    N = random.randrange(2, 10)
+
+    print("M = ", M, "; N = ", N)
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = j*i
+
+    print(a, '\n', '\n')
+
+    '''
+    Выводим элементы с четными номерами из всех строк:
+    '''
+    for i in range(M):
+        for j in range(0, N, 2):
+            print(f'Номер элемента {j}, строка {i} - ', a[i][j])
+
+    '''
+    Выводим элементы из строк с четными номерами:
+    '''
+    print()
+
+    for i in range(0, M, 2):
+        for j in range(0, N):
+            print(f'Номер элемента {j}, строка {i} - ', a[i][j])
+
+
+def N10():
+    M = random.randrange(2, 10)
+    N = random.randrange(2, 10)
+
+    print("M = ", M, "; N = ", N)
+    a = np.zeros((M, N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = j*i
+
+    print(a, '\n', '\n')
+
+    '''
+    Выводим элементы с нечетными номерами из всех столбцов:
+    '''
+    for i in range(N):
+        for j in range(1, M, 2):
+            print(f'Номер элемента {j}, столбец {i} - ', a[j][i])
+
+    '''
+    Выводим элементы из столбцов с нечетными номерами:
+    '''
+    print()
+
+    for i in range(1, N, 2):
+        for j in range(M):
+            print(f'Номер элемента {j}, столбец {i} - ', a[j][i])
+
+
+def N11():
+    M = random.randint(2, 10)
+    N = random.randint(2, 10)
+
+    print("M = ", M, "; N = ", N)
+    a = np.zeros((M,N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = j*i
+
+    print(a, '\n', '\n')
+
+
+    for i in range(M):
+        if i%2 == 0:
+            for j in range(N):
+                print(a[i][j], end = ', ' )
+        else:
+            for j in range(N, 0, -1):
+                k = j - 1
+                print(a[i][k], end = ', ')
+        print()
+
+
+def N12():
+    M = random.randint(2, 10)
+    N = random.randint(2, 10)
+
+    print("M = ", M, "; N = ", N)
+    a = np.zeros((M,N))
+    for i in range(M):
+        for j in range(N):
+            a[i][j] = j*i
+
+    print(a, '\n', '\n')
+
+
+    for i in range(M):
+            for j in range(N):
+                if j%2 == 0:
+                    print(a[i][j], end = ', ')
+
+                else:
+                    print(a[M-1-i][j], end = ', ')
+            print()
+
+
+def N13():
+    M = random.randint(2, 10)
+    print('M = ', M)
+
+    a = np.zeros((M, M))
+    for i in range(M):
+        for j in range(M):
+            a[i][j] = j-1*i*2
+
+    print(a)
+
+    for i in range(M):
+        for j in range(M-i):
+            print(a[i][j], end = ', ')
+            if j == M-i-1:
+                print('столбец', end = ', ')
+                for k in range(i+1, M):
+                    print(a[k][j], end = ', ')
+        print()
+
+
+def N14():
+    M = random.randint(2, 10)
+    print('M = ', M)
+
+    a = np.zeros((M, M))
+    for i in range(M):
+        for j in range(M):
+            a[i][j] = j*i
+    print(a)
+
+    for i in range(M):
+        for j in range(M-i):
+            print(a[j][i], end = ', ')
+            if j == M-i-1:
+                print('строка', end = ', ')
+                for k in range(i+1, M):
+                    print(a[j][k], end = ', ')
+        print()
+
+
+def N15():
+    M = 0
+    while M%2 == 0 :
+        M = random.randint(2, 11)
+    print('M = ', M)
+
+    a = np.zeros((M, M))
+    for i in range(M):
+        for j in range(M):
+            a[i][j] = j-1*i*2
+    print(a)
+
+    k = 0
+    while k <= M//2:
+        j = 0 + k
+        #print('Горизонталь слева - направо:')
+        for i in range(0+k, M-k):
+            print(a[j][i], end = ', ')
+            if i == M - 1 - k:
+                print()
+                #print('Вертикаль сверху - вниз:')
+                for j in range(1+k, M-k):
+                    print(a[j][i], end = ', ')
+                    if j == M - 1 - k:
+                        print()
+                        #print('Горизонталь справа - налево:')
+                        for i in range(M-1-k, 0+k, -1):
+                            v = i - 1
+                            print(a[j][v], end = ', ')
+                            if v == 0 + k:
+                                print()
+                                #print('Горизонталь снизу - вверх:')
+                                for j in range(M-1-k, 1+k, -1):
+                                    w = j - 1
+                                    print(a[w][v], end = ', ')
+                                print()
+
+        k+=1
+
+
+
+def N16():
+    M = 0
+    while M%2 == 0 :
+        M = random.randint(2, 5)
+    print('M = ', M)
+
+    a = np.zeros((M, M))
+    for i in range(M):
+        for j in range(M):
+            a[i][j] = j-1*i*2
+    print(a)
+
+    k = 0
+    while k <= M//2:
+        j = 0 + k
+        #print('Горизонталь слева - направо:')
+        for i in range(0+k, M-k):
+            print(a[i][j], end = ', ')
+            if i == M - 1 - k:
+                print()
+                #print('Вертикаль сверху - вниз:')
+                for j in range(1+k, M-k):
+                    print(a[i][j], end = ', ')
+                    if j == M - 1 - k:
+                        print()
+                        #print('Горизонталь справа - налево:')
+                        for i in range(M-1-k, 0+k, -1):
+                            v = i - 1
+                            print(a[v][j], end = ', ')
+                            if v == 0 + k:
+                                print()
+                                #print('Горизонталь снизу - вверх:')
+                                for j in range(M-1-k, 1+k, -1):
+                                    w = j - 1
+                                    print(a[v][w], end = ', ')
+                                print()
+
+        k+=1
+
+
+def N17():
+    pass
+
+
+
+
+
+N17()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
